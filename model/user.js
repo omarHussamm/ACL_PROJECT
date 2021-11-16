9a435c7113ff552c94331056f4eadafbe26838e4
Ahmed El-Etreiby <aetreiby7@gmail.com>
2021-11-16 22:22:38 +0200

    jjj

---
+++ model/user.js
@@ -0,0 +1,11 @@
+class user {
+    
+
+
+    constructor( username,  email,  password, telephoneNum){
+     this.username=username;
+     this.email=email;
+     this.password= password;
+     this.telephoneNum= telephoneNum;
+    }
+}
