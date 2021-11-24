342cdb7aa87787250bf926919c42be179dcb7454
omarHussamm <83400756+omarHussamm@users.noreply.github.com>
2021-11-24 04:17:15 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,30 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from './components/loginForm';
 import AdminHome from './components/adminHome';
-import button from './components/adminHome';
+import CreateFlight from './components/createFlight';
+import React from 'react';
 
-function App() {
-  return (
-    <div>lol
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<LoginForm/>} />
-        <Route path="/admin" exact element={<AdminHome/>} />
-      </Routes>
-    </Router>
-    </div>
-  );
+class App extends React.Component {
+
+
+
+  render() {
+    return (
+      <div>
+
+        <AdminHome/>
+        {/* <Router>
+          <Routes>
+            <Route path="/" exact element={<LoginForm />} />
+            <Route path="/admin" exact element={<AdminHome />} />
+            <Route path="/admin/createFlight" exact element={<CreateFlight />} />
+
+          </Routes>
+        </Router> */}
+      </div>
+    )
+  }
+
 }
 
 export default App;
