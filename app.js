fd472bc97f6b8214219f8233db1c08a376a73b2f
omarHussamm <83400756+omarHussamm@users.noreply.github.com>
2021-11-24 04:15:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,42 +11,23 @@ import connectDB from './config/db.js';
 dotenv.config();
 
 const app = express();
-
-
 app.use(express.json());
 connectDB();
-app.use(cors());
+
+app.use(cors())
+
 app.use('/admin', AdminRoutes);
 app.use('/flights', flightRoutes);
 
 const PORT = process.env.PORT || 5000
 
 User.find().then(users => {
- console.log("users",users[0]);
+ console.log("users",users);
 });
 
 Flight.find().then(flights => {
-  console.log("flights",flights[1]);
+  console.log("flights",flights);
  });
  
 
 app.listen(PORT);
-app.get('/', (req,res) => {
-res.send('hi')
-})
-app.get('/home',(req,res)=>{
-app.post('/admin',(req,res)=>{
-  Flight.find().then(flights => {
-    
-   
-   err_msg = "flights";
-   res.render('admin', { err: err_msg });
-   res.end;
-   return;
-  });
-
-  
-}
-  
-)
-})
